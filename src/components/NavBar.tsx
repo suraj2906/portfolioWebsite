b491d8c6c7bcfa992baf1cef63fc11bbdc3ba20c
Suraj Shah <surajshah.bombay@gmail.com>
2024-08-27 16:25:15 +0530

    Made NavBar responsive

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -6,7 +6,7 @@ type Props = {}
 
 export const NavBar = (props: Props) => {
   return (
-    <div className='absolute top-0 md:px-[10%] w-[100%] py-[3%] px-[5%] font-cruell text-3xl'>
+    <div className='absolute top-0 md:px-[10%] w-[100%] md:py-[3%] px-[5%] py-[10%] font-cruell md:text-3xl text-[12px]'>
         <motion.ul  
         initial={{opacity: 0}}
         animate={{opacity: 1}}
@@ -14,7 +14,7 @@ export const NavBar = (props: Props) => {
         className='flex flex-row justify-between text-white'>
             <motion.li><a href='#about'>ABOUT</a></motion.li>
             <motion.li><a href='#projects'>WORK</a></motion.li>
-            <motion.li className='font-vintage text-6xl border solid rounded-full p-5 -mt-5'><a href='https://github.com/suraj2906' target='_blank'>SS</a></motion.li>
+            <motion.li className='font-vintage md:text-6xl text-[15px] border solid rounded-full w-12 h-12 md:w-auto md:h-auto flex items-center justify-center md:p-5 p-3 md:-mt-5 -mt-2'><a href='https://github.com/suraj2906' target='_blank'>SS</a></motion.li>
             <motion.li>SOCIALS</motion.li>
             <motion.li>CONTACT</motion.li>
         </motion.ul>
