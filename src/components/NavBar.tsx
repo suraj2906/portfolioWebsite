c62f87f7b26199de022a30dd0055ab59926b3f08
Suraj Shah <surajshah.bombay@gmail.com>
2024-08-27 16:58:13 +0530

    Added react-scroll for smooth scrolling, fixed initial jump to section

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react'
 import {motion} from 'framer-motion'
+import { Link, animateScroll as scroll } from "react-scroll"
 
 type Props = {}
 
@@ -13,8 +14,8 @@ export const NavBar = (props: Props) => {
         transition={{ duration: 2 }}
         className='flex flex-row justify-between text-white'>
             <motion.li><a href='#about'>ABOUT</a></motion.li>
-            <motion.li><a href='#projects'>WORK</a></motion.li>
-            <motion.li className='font-vintage md:text-6xl text-[15px] border solid rounded-full w-12 h-12 md:w-auto md:h-auto flex items-center justify-center md:p-5 p-3 md:-mt-5 -mt-2'><a href='https://github.com/suraj2906' target='_blank'>SS</a></motion.li>
+            <motion.li><Link to='projects'>WORK</Link></motion.li>
+            <a href='https://github.com/suraj2906' target='_blank'><motion.li className='font-vintage md:text-6xl text-[15px] border solid rounded-full w-12 h-12 md:w-auto md:h-auto flex items-center justify-center md:p-5 p-3 md:-mt-5 -mt-2'>SS</motion.li></a>
             <motion.li>SOCIALS</motion.li>
             <motion.li>CONTACT</motion.li>
         </motion.ul>
